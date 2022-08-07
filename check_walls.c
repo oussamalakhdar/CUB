05b0fb780a3d749c9dfb04fb25831f3b276a4852
Oussama Lakhdar <lakhdaroussama99@gmail.com>
2022-08-07 16:44:42 +0100

    close

--- check_walls.c
+++ check_walls.c
@@ -3,14 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   check_walls.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abayar <abayar@student.42.fr>              +#+  +:+       +#+        */
+/*   By: olakhdar <olakhdar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 13:59:33 by abayar            #+#    #+#             */
-/*   Updated: 2022/08/07 13:55:15 by abayar           ###   ########.fr       */
+/*   Updated: 2022/08/07 16:42:00 by olakhdar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+int	check_first(char **s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i][0] != '1' && s[i][0] != ' ')
+		{
+			printf("-- %c", s[i][0]);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	check_char(char *s)
+{
+	int	i;
+
+	i = 0;
+	while(s[i])
+	{
+		if (s[i] != '0' && s[i] != '1' && s[i] != 'N' && s[i] != 'S' && s[i] != 'E' && s[i] != 'W' && s[i] != ' ')
+			return (0);
+		i++;
+	}
+	return(1);
+}
 
 int check_cas(char *col, char **s, int x, int y)
 {
@@ -46,7 +77,11 @@ int check_cas(char *col, char **s, int x, int y)
 				printf("4  --[%c]\n", col[y]);
 				return (0);
 			}
-			// else if ()
+			else if (s[x + 1][y] == '0' || col[y + 1] == '0' || s[x + 1][y + 1] == '0' || s[x - 1][y] == '0' || s[x - 1][y + 1] == '0'|| s[x - 1][y - 1] == '0'|| col[y - 1] == '0' || s[x - 1][y - 1] == '0')
+			{
+				printf("15  --[[%c][%d][%d][%c]\n", s[x + 1][y], x, y, col[y + 1]);
+				return (0);
+			}
 		}
 		else if (col[y] == '1')
 		{
@@ -60,16 +95,9 @@ int check_cas(char *col, char **s, int x, int y)
 				printf("6  --[%c]\n", s[x + 1][y]);
 				return (0);
 			}
-			// else if (s[x + 1][y] != ' ' && col[y + 1] != '1' && s[x + 1][y + 1] == ' ')
-			// {
-			// 	printf("7  --[%c][%d][%d][%c]\n", s[x + 1][y], x, y, col[y + 1]);
-			// 	return (0);
-			// }
 		}
-		// else
-		// 	return (0);
 	}
-	printf("x= %d y =%d\n", x,y);
+	// printf("x= %d y =%d\n", x,y);
 	return (1);
 }
 
@@ -96,8 +124,12 @@ int pars_loop(char **s)
 	i = 0;
 	if (!s)
 		return (0);
+	if(!check_first(s))
+		return (0);
 	while (s[i])
 	{
+		if(!check_char(s[i]))
+			return (0);
 		if (!check_col(s[i], s, i))
 			return (0);
 		i++;
